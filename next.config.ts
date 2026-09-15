dfc49275bc1db2627b93d38292a43e762b1114bb
bentry95 <97233132+bentry95@users.noreply.github.com>
2026-09-15 15:35:19 +0200

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,33 @@
+
+import type { NextConfig } from 'next';
+
+const securityHeaders = [
+  {
+    key: 'Content-Security-Policy',
+    value: "frame-ancestors *;",
+  }
+];
+
+const nextConfig: NextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'placehold.co', pathname: '/**' },
+      { protocol: 'https', hostname: 'images.unsplash.com', pathname: '/**' },
+      { protocol: 'https', hostname: 'picsum.photos', pathname: '/**' },
+      { protocol: 'https', hostname: 'firebasestorage.googleapis.com', pathname: '/**' }
+    ],
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ]
+  },
+};
+
+export default nextConfig;
